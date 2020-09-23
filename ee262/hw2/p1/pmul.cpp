d4da7bb9225fe4047b78e5c0a7a01ed117a411de
BuildTools <unconfigured@null.spigotmc.org>
2020-09-23 18:11:17 -0400

    Commit by Cam on September 23, 2020 18:11:17

--- ee262/hw2/p1/pmul.cpp
+++ ee262/hw2/p1/pmul.cpp
@@ -9,8 +9,8 @@
 // Function defs
 void ip_multiply(); // Multiplies two polynomials and prints out the result to the screen
 void getUserInput(); // Gets information from the user
-void printPolynomial(std::vector <int> vec); // Prints a polynomial 
-void printPolynomialTraditional(std::vector <int> vec); // Prints a polynomial in a traditional math form
+void printPolynomial(std::vector <int> vec); // Prints a polynomial as a list of numbers 
+void printPolynomialTraditional(std::vector <int> vec); // Prints a polynomial in traditional math form
 
 // Define variables for polynomial-related info, and vectors to contain the polynomials themselves
 int p1Degree = 0;
@@ -22,32 +22,33 @@ std::vector <int> p3(0); // The multiplied polynomial
 
 
 int main(){
+	getUserInput(); // Get the polynomials from the user
+	ip_multiply(); // Multiply the polynomials
 
-	
-	getUserInput();
-	ip_multiply();
+	// Print the result in both forms
+	std::cout << "Product -- "; 
 	printPolynomialTraditional(p3);
+	printPolynomial(p3);
 	
-	
-
-
 	return 0;
 
 }
 
+
+// Multiples the two polynomials together and stores the result in p3
 void ip_multiply(){
 
-	int firstVal;
-	int secondVal;
-	int multVal;
-	int degreeOfMultVal;
-	int tempValFromPoly;
+	int firstVal; // The first value to multiply
+	int secondVal; // The second value to multiply
+	int multVal; // The multipled value
+	int degreeOfMultVal; // The degree of the multipled value (ex. degree of 3 translates to multVal*x^3)
 
+	// Multiply out the polynomials, taking the each term from the first polynomial and multiplying it by each term of the second,
+	// then moving to the second term from the first polynomial and so on
 	for (int i = 0; i <= p1Degree; i++){
 		firstVal = p1[i];
 		for (int j = 0; j <= p2Degree; j++){
 			degreeOfMultVal = i + j;
-			tempValFromPoly = p3[degreeOfMultVal];
 			secondVal = p2[j];
 			multVal = firstVal * secondVal;
 			p3[degreeOfMultVal] = p3[degreeOfMultVal] + multVal;
@@ -60,10 +61,15 @@ void ip_multiply(){
 	return;
 }
 
+
+// Takes in the user's choice of two polynomials
 void getUserInput(){
+
+	// Start by getting the first polynomial from the user
 	std::cout << "Enter the degree of p1: \n> ";
 	std::cin >> p1Degree;
 
+	// Get all coefficients of the first polynomial
 	for (int i = 0; i <= p1Degree; i++){
 		int tempVal = 0;
 		std::cout << "[p1] coefficient of x^" << i << ":\n> ";
@@ -72,12 +78,19 @@ void getUserInput(){
 		p1.push_back(tempVal);
 
 	}
-
+	std::cout << std::endl;
+	printPolynomialTraditional(p1);
 	std::cout << std::endl;
 
+
+	std::cout << std::endl; // Provide a space in the input to make the input stream a bit clearer
+
+
+	// Get the second polynomial from the user
 	std::cout << "Enter the degree of p2: \n> ";
 	std::cin >> p2Degree;
 
+	// Get all coefficients for the second polynomial
 	for (int i = 0; i <= p2Degree; i++){
 		int tempVal = 0;
 		std::cout << "[p2] coefficient of x^" << i << ":\n> ";
@@ -86,6 +99,10 @@ void getUserInput(){
 		p2.push_back(tempVal);
 
 	}
+
+	std::cout << std::endl;
+	printPolynomialTraditional(p2);
+	std::cout << std::endl;
 
 
 	// Init p3 with the correct amount of zeros
@@ -97,23 +114,23 @@ void getUserInput(){
 
 }
 
-void printPolynomial(std::vector <int> vec){
-	std::cout << " Product -- [";
-	for(int i = 0; i <= vec.size(); i++){
-		
 
-		if (i == vec.size()){
-			std::cout << vec[i];
-		}else{
+void printPolynomial(std::vector <int> vec){
+	std::cout << "[";
+	for(int i = 0; i <= vec.size() - 1; i++){
+		if (i != vec.size()-1){
 			std::cout << vec[i] << ", ";
+		}else{
+			std::cout << vec[i];
 		}
 	}
 	std::cout << "]";
 	return;
 }
 
+
 void printPolynomialTraditional(std::vector <int> vec){
-	for(int i = 0; i <= vec.size(); i++){
+	for(int i = 0; i <= vec.size() - 1; i++){
 		if (vec[i] != 0){
 			switch(i){
 				case 0:
@@ -128,6 +145,8 @@ void printPolynomialTraditional(std::vector <int> vec){
 
 			if (i != vec.size()-1){
 				std::cout << " + ";
+			}else{
+				std::cout << std::endl;
 			}
 		}
 	}
