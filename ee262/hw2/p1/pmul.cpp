bf2c59385e6ad5ad6e3111d9abdc91d8da282858
palmtrey <palmtrey779@gmail.com>
2020-09-23 15:13:55 -0400

    Commit by Cam on September 23, 2020 15:13:55

---
+++ ee262/hw2/p1/pmul.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+
+
+// Function defs
+void ip_multiply(); // Multiplies two polynomials and prints out the result to the screen
+
+
+
+int main(){
+
+	int p1Degree = 0;
+	int p2Degree = 0;
+
+	std::cout << "Enter the degree of p1: \n>"
+	std::cin >> p1Degree;
+	
+
+
+	return 0;
+
+}
