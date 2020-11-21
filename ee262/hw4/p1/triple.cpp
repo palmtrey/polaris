3fb969f68a4ee74dbc5dd4f490481d48b68419c6
palmtrey <palmtrey779@gmail.com>
2020-11-21 18:26:44 -0500

    Commit by Cam on November 21, 2020 18:26:44

--- ee262/hw4/p1/triple.cpp
+++ ee262/hw4/p1/triple.cpp
@@ -1,4 +1,44 @@
-//
-// Created by Cameron Palmer on 11/21/20.
-//
+// Title: Triple.cpp
+// Purpose: To provide an implementation file for triple.h
+// Developers: Cameron Palmer, Shawn Boyd
+// Last Modified: November 21st, 2020
 
+#include "triple.h"
+
+using namespace simSequence;
+
+template <typename T>
+Triple<T>::Triple(T first, T second, T third) {
+    Triple<T>::first = first;
+    Triple<T>::second = second;
+    Triple<T>::third = third;
+    Triple<T>::name = "unnamed";
+}
+
+template <typename T>
+Triple<T>::Triple(T first, T second, T third, string name) {
+    Triple<T>::first = first;
+    Triple<T>::second = second;
+    Triple<T>::third = third;
+    Triple<T>::name = name;
+}
+
+template <typename T>
+T Triple<T>::getFirst() {
+    return first;
+}
+
+template <typename T>
+T Triple<T>::getSecond() {
+    return second;
+}
+
+template <typename T>
+T Triple<T>::getThird() {
+    return third;
+}
+
+template <typename T>
+void Triple<T>::print() {
+    std::cout << name << ": (" << first << ", " << second << ", " << third << ")" << std::endl;
+}
